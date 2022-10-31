2f64495df44769cb1b58dae551f52c576b6b8fde
mslilyb <lilith.r.m.d@gmail.com>
2022-10-31 08:47:25 -0700

    up to aacomp done

---
+++ aacomp.py
@@ -0,0 +1,19 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(description = "program to report amino acid composition alphabetically")
+
+parser.add_argument("seq", metavar='str', type=str, help="sequence to find the composition of")
+
+args = parser.parse_args()
+
+aas = {}
+
+for aa in args.seq:
+	if aa in aas:
+		aas[aa] += 1
+	else:
+		aas[aa] = 1
+
+for aacid in sorted(aas):
+	print(aacid,":",aas[aacid])
