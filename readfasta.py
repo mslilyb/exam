3cbd23f79c3d89f24b06eea8591d51ab3b8217b0
mslilyb <lilith.r.m.d@gmail.com>
2022-11-03 09:58:23 -0700

    through readfasta done

---
+++ readfasta.py
@@ -0,0 +1,32 @@
+import gzip
+
+
+def readfasta(fname):
+	fp = None
+	seqname = ""
+	seq = []
+
+	if fname.endswith(".gz"): fp = gzip.open(fname, mode = 'rt')
+	else:					  fp = open(fname)
+
+	while True:
+		line = fp.readline()
+		if line = '': break
+
+		line = line.rstrip()
+
+		if line.startswith(">"):
+
+			if len(seq) > 0:
+				yield(seqname, ''.join(seq))
+
+				seqname = line[1:]
+				seq = []
+			else:
+				seqname = line[1:]
+		else:
+			seq.append(line)
+			
+	yield(seqname, ''.join(seq))
+
+	fp.close()
